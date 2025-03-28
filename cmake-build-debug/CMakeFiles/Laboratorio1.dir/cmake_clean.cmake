f79eeafd5675e0751ddf18e393a022757f003b9d
Agustin Machado <agusm.s.2006@gmail.com>
2025-03-27 23:11:13 -0300

    main

---
+++ cmake-build-debug/CMakeFiles/Laboratorio1.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laboratorio1.dir/DTFecha.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/DTFecha.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/DTinfoEstudiante.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/DTinfoEstudiante.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/Estudiante.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/Estudiante.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/Informacion.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/Informacion.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/Libro.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/Libro.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/PaginaWeb.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/PaginaWeb.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/chatGPT.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/chatGPT.cpp.obj.d"
+  "CMakeFiles/Laboratorio1.dir/main.cpp.obj"
+  "CMakeFiles/Laboratorio1.dir/main.cpp.obj.d"
+  "Laboratorio1.exe"
+  "Laboratorio1.exe.manifest"
+  "Laboratorio1.pdb"
+  "libLaboratorio1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laboratorio1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
